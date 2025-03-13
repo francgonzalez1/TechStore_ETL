53cd246b81027959c9b1d07f2591fae038428dfe
Francisco Gonzalez <francg14@hotmail.com>
2025-03-13 10:13:04 -0600

    Update total-sales-by-client.sql

--- total-sales-by-client.sql
+++ total-sales-by-client.sql
@@ -1,1 +1,8 @@
+USE TechStore;
+
+SELECT c.nombre AS Cliente, SUM(v.cantidad * p.precio) AS Total_Ventas
+FROM Ventas v
+JOIN Clientes c ON v.id_cliente = c.id_cliente
+JOIN Productos p ON v.id_producto = p.id_producto
+GROUP BY c.nombre;
 
