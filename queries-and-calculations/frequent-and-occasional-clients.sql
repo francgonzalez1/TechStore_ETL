484e34a0329a5348555a3fd3d21f1593bf4ed0af
francgonzalez1 <francg14@hotmail.com>
2025-03-13 11:43:29 -0600

    Organizacion completa del Proyecto TechStore_ETL

--- queries-and-calculations/frequent-and-occasional-clients.sql
+++ queries-and-calculations/frequent-and-occasional-clients.sql
@@ -1,1 +1,11 @@
+USE TechStore;
+
+SELECT c.nombre AS Cliente, COUNT(v.id_venta) AS Numero_Compras,
+       CASE
+           WHEN COUNT(v.id_venta) >= 5 THEN 'Frecuente'
+           ELSE 'Ocasional'
+       END AS Clasificacion
+FROM Ventas v
+JOIN Clientes c ON v.id_cliente = c.id_cliente
+GROUP BY c.nombre;
 
