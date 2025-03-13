d4f79ae6a645f0cccd19c959cf3826e0107d6756
Francisco Gonzalez <francg14@hotmail.com>
2025-03-13 11:51:55 -0600

    Update total-sales-by-product.sql

--- queries-and-calculations/total-sales-by-product.sql
+++ queries-and-calculations/total-sales-by-product.sql
@@ -1,1 +1,6 @@
+USE TechStore;
 
+SELECT p.nombre AS Producto, SUM(v.cantidad) AS Unidades_Vendidas, SUM(v.cantidad * p.precio) AS Total_Ventas
+FROM Ventas v
+JOIN Productos p ON v.id_producto = p.id_producto
+GROUP BY p.nombre;
