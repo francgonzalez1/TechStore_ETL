d4ae215e98be9c9ead42af98b563622372910efe
Francisco Gonzalez <francg14@hotmail.com>
2025-03-13 11:51:23 -0600

    Update top-and-least-sold-products.sql

--- queries-and-calculations/top-and-least-sold-products.sql
+++ queries-and-calculations/top-and-least-sold-products.sql
@@ -1,1 +1,11 @@
+USE TechStore;
+
+SELECT p.nombre AS Producto, SUM(v.cantidad) AS Total_Unidades,
+       CASE
+           WHEN SUM(v.cantidad) >= 50 THEN 'Top Vendido'
+           ELSE 'Menos Vendido'
+       END AS Clasificacion
+FROM Ventas v
+JOIN Productos p ON v.id_producto = p.id_producto
+GROUP BY p.nombre;
 
